d904297c98293cbb0eec147b970f916cbaaf7823
Ugur Demiriz <udemiriz35@gmail.com>
2025-12-10 22:21:10 +0300

    tailwindcss.config.js düzeltilmişti tekrar postcss.config.js üzerinde son kez değişilkil yaptım.

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
+// postcss.config.js
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: {
+  plugins: [
     tailwindcss,
     autoprefixer,
-  },
-};
+  ],
+}

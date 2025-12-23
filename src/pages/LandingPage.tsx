dcf543926a157e753c6cad455a81522f3db39141
Ugur Demiriz <udemiriz35@gmail.com>
2025-12-23 16:49:50 +0300

    Bundan sonra src/pages.. src/components içindekiler için hata lamayacak değişim yapıldı.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 // src/pages/LandingPage.tsx
+import Header from "../components/Header";
 import React from "react";
-// import Header from "../components/Header";
 import Container from "../components/Container";
 import Card from "../components/Card";
 import Button from "../components/Button";
--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LandingPage from "./LandingPage";
+import LandingPage from "../pages/LandingPage";
 
 const Home: React.FC = () => {
   return <LandingPage />;

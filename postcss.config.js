9298a44a06c3cb4c6ba45f0d96a4cdc7ce83a114
Ugur Demiriz <udemiriz35@gmail.com>
2025-12-25 12:45:20 +0300

    Tailwind v4 PostCSS konfigürasyonu gerçekleitrildi.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
-// postcss.config.js
-import tailwindcss from '@tailwindcss/postcss'
+// postcss.config.js@import "tailwindcss";
+import "tailwindcss";
 import autoprefixer from 'autoprefixer'
 
 export default {
